84e6ae2ef228a3f32d095acbc2cea7d516bc4e1c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-04-24 19:28:08 +0100

    add deocorator for proper status code on login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto, AuthSignInDto } from '../dto';
 import { Request } from 'express';
@@ -13,6 +13,7 @@ export class AuthController {
     return this.authService.signUp(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() dto: AuthSignInDto) {
     return this.authService.login(dto);
